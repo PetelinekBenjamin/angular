0785903ac33b004776f19f1be85bf931b62dc952
Benjamin <benjamin.petelinek@gmail.com>
2024-07-31 13:40:55 +0200

    commit

--- hello-world/src/app/components/pages/home/home.component.ts
+++ hello-world/src/app/components/pages/home/home.component.ts
@@ -4,12 +4,15 @@ import { Food } from '../../../shared/models/Food';
 import { FoodService } from '../../../services/food.service';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from "../../partials/search/search.component";
+import { TagsComponent } from "../../partials/tags/tags.component";
+import { NotFoundComponent } from "../../partials/not-found/not-found.component";
+import { Observable } from 'rxjs';
 // Import RatingModule if it is available and necessary
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule, SearchComponent],  // Ensure imports are correct
+  imports: [RouterModule, CommonModule, SearchComponent, TagsComponent, NotFoundComponent],  // Ensure imports are correct
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -17,12 +20,28 @@ export class HomeComponent {
   foods: Food[] = [];
 
   constructor(private foodService: FoodService, activatedRoute:ActivatedRoute) {
+    let foodsObservable: Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
       if(params.searchTerm){
-        this.foods = this.foodService.getAllbySearchTerm(params.searchTerm);
+        foodsObservable = this.foodService.getAllbySearchTerm(params.searchTerm);
+
+        foodsObservable.subscribe((serverFood)=>{
+          this.foods = serverFood
+        })
+      }
+      else if(params.tag){
+        foodsObservable = this.foodService.getAllFoodsByTag(params.tag);
+
+        foodsObservable.subscribe((serverFood)=>{
+          this.foods = serverFood
+        })
       }
       else{
-        this.foods = foodService.getAll();
+        foodsObservable = foodService.getAll();
+
+        foodsObservable.subscribe((serverFood)=>{
+          this.foods = serverFood
+        })
       }
 
     });
--- hello-world/src/app/components/partials/header/header.component.ts
+++ hello-world/src/app/components/partials/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  cartQuantity = 0;
+
+  constructor(cartService:CartService){
+    cartService.getCartObservable().subscribe(cart => this.cartQuantity = cart.totalCount);
+  }
 
 }
